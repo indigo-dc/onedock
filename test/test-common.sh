a7723d5005569a2760ff390ca484a752dadabb11
Carlos A <caralla@upv.es>
2015-12-04 14:10:12 +0100

    working on tests

--- test/test-common.sh
+++ test/test-common.sh
@@ -60,4 +60,52 @@ function wait_for_timeout_nequal()
         done
 }
 
+function create_vm() {
+	FILENAME=/tmp/onedock.vm
+	[ "$1" != "" ] && FILENAME=$1
 
+	progress
+
+	# Create a vm
+	VMID=$(onevm create "$FILENAME" | awk '{print $2}')
+	export VMID
+	
+	progress
+	
+	# Froce deploying just in case that it has not enough cores
+	onevm deploy $VMID $HOSTNAME
+	
+	progress
+	
+	# Check pending state
+	CMD="onevm show -x $VMID | /var/lib/one/remotes/datastore/xpath.rb /VM/STATE"
+	wait_for_timeout_equal 60 1 "$CMD" "1"
+	
+	progress
+	
+	# Wait a little more, until running state
+	wait_for_timeout_nequal 60 1 "onevm show -x $VMID | /var/lib/one/remotes/datastore/xpath.rb /VM/STATE" "3"
+	
+	progress
+	
+	# Check running state
+	check_cmd_neq "$CMD" "3" && exit 1
+	
+	progress
+	
+	wait_for 10
+	
+	# Do a ping to check the machine
+	IP=$(onevm show -x $VMID | /var/lib/one/remotes/datastore/xpath.rb /VM/TEMPLATE/NIC/IP)
+	ping -c 3 $IP > /dev/null
+	
+	progress
+	
+	# Check the docker containers
+	[ "$(docker ps | grep one-$VMID)" == "" ] && exit 1
+	# Check the state of the container 
+	[ "$(docker inspect -f '{{.State.Running}}' one-$VMID)" == "" ] && exit 1
+	# Check the images repository
+	[ "$(docker images | grep "one-$VMID")" == "" ] && exit 1
+	return 0	
+}
