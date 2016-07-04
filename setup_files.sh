6ecd267734e350807c7aaee4a8083fabefc771bc
alpegon <alpegon3@upvnet.upv.es>
2016-07-04 15:25:47 +0200

    Update config file installation

--- setup_files.sh
+++ setup_files.sh
@@ -37,32 +37,8 @@ cp $BASEDIR/onedock.sh /var/lib/one/remotes/
 cp $BASEDIR/docker-manage-network /var/lib/one/remotes/
 
 if [ ! -e /var/lib/one/remotes/onedock.conf ]; then
-cat > /var/lib/one/remotes/onedock.conf << EOF
-#!/bin/bash
-#
-# ONEDock - Docker support for ONE (as VMs)
-# Copyright (C) GRyCAP - I3M - UPV 
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-export LOCAL_SERVER=$(hostname):5000
-export DATASTORE_DATA_PATH=/var/lib/docker-registry/data/
-export ONEDOCK_LOGFILE=/var/log/onedock.log
-export IMAGE_BASENAME=image4one
-export ONEDOCK_DEBUG=True
-export ONEDOCK_DEFAULT_NETMASK=24
-export ONEDOCK_DEFAULT_DHCP=yes
-EOF
+    cp onedock.conf /var/lib/one/remotes/onedock.conf
+    sed -i.bkp -e "s/export LOCAL_SERVER=.*/export LOCAL_SERVER=$(hostname):5000/g" /var/lib/one/remotes/onedock.conf
 else
 echo "Configuration file /var/lib/one/remotes/onedock.conf exists. \
     Please check that the variables are properly set."
