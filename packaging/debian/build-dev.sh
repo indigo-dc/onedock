2ac605680d0d91adbac9f0a37a96db3a1b181183
Carlos A <caralla@upv.es>
2016-07-07 13:38:40 +0000

    creating packages for debian-based systems

---
+++ packaging/debian/build-dev.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+NODE=master
+VERSION=1.0-1
+APP=onedock
+BASE_FOLDER=.
+
+FILELIST="vmm im datastore tm onedock.sh onedock.conf docker-manage-network"
+
+function copy_files {
+	mkdir -p ./dist/$BUILD_PKG/var/lib/one/remotes/
+	for f in $FILELIST; do
+		cp -r $BASE_FOLDER/$f ./dist/$BUILD_PKG/var/lib/one/remotes/
+	done
+}
+
+while [ $# -gt 0 ]; do
+	case $1 in
+		-f)	BASE_FOLDER=$2
+			shift
+			shift;;
+		*)	echo "unexpected parameter $1" >&2
+			exit 1;;
+
+esac
+done
+rm -rf dist
+BUILD_PKG=${APP}_${VERSION}_master
+mkdir -p ./dist/$BUILD_PKG/
+copy_files
+cp -r master/DEBIAN ./dist/$BUILD_PKG
+dpkg-deb --build ./dist/$BUILD_PKG
+
+BUILD_PKG=${APP}_${VERSION}_node
+mkdir -p ./dist/$BUILD_PKG/
+cp -r node/DEBIAN/ ./dist/$BUILD_PKG/
+dpkg-deb --build ./dist/$BUILD_PKG
